f696a711812dbdc876730b28f7e8c2e2a165b34f
hassanGIT <mh1673345@gmail.com>
2026-07-26 16:05:54 +0500

    chore: make database SSL connection configurable and update environment templates

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    ssl: {
+    ssl: process.env.DB_SSL === 'false' ? undefined : {
         rejectUnauthorized: false
     },
     waitForConnections: true,
